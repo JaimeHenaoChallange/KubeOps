bd42928521ba67aa0b199af17f631ae475e54b7b
jaimehenao8126 <148729312+jaimehenao8126@users.noreply.github.com>
2024-07-02 10:58:05 -0500

    Update docker-entrypoint.sh

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-# Configuración adicional antes de iniciar Nginx
+# Configurar el entorno si es necesario (opcional)
 echo "Configurando el entorno..."
-
-# Si necesitas configurar algo específico, como variables de entorno o configuraciones adicionales,
-# puedes hacerlo aquí antes de iniciar Nginx.
 
 # Iniciar Nginx
 echo "Iniciando Nginx..."
 exec nginx -g 'daemon off;'
+
